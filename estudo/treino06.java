89456fade4d65e84e764911eb0ad3717742ef9e6
Joao <engjobe@gmail.com>
2022-02-28 20:14:55 -0300

    beecrowd 1007

---
+++ estudo/treino06.java
@@ -0,0 +1,22 @@
+package estudo;
+
+import java.util.Scanner;
+
+public class treino06 {
+    public static void main (String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a, b, c , d, diferenca;
+
+        a = entrada.nextInt();
+        b = entrada.nextInt();
+        c = entrada.nextInt();
+        d = entrada.nextInt();
+
+        diferenca = (a * b - c * d);
+
+        System.out.println("DIFERENCA = " + diferenca);
+
+        entrada.close ();
+    }
+    
+}
