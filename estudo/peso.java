4e188cf4571366cbd9eaf1abef0bec0fffda7aa2
Joao <engjobe@gmail.com>
2022-03-17 10:14:01 -0300

    Qual seria o seu peso em outros mundos?

--- estudo/peso.java
+++ estudo/peso.java
@@ -6,20 +6,19 @@ import java.util.Locale;
 public class peso {
     public static void main (String[] args) {
         Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
-        System.out.println("Qual seria o seu peso em outros mundos?\n");
         System.out.println("Digite o seu peso em Kg");
-        double seuPeso = entrada.nextDouble();
+        double peso = entrada.nextDouble();
 
-        System.out.printf("Mercúrio: %.1f Kg\n", seuPeso * 0.37);
-        System.out.printf("Vênus: %.1f Kg\n", seuPeso * 0.88);
-        System.out.printf("Terra: %.1f Kg\n", seuPeso);
-        System.out.printf("Lua: %.1f Kg\n", seuPeso * 0.1);
-        System.out.printf("Marte: %.1f Kg\n", seuPeso * 0.38);
-        System.out.printf("Júpiter: %.1f Kg\n", seuPeso * 2.64);
-        System.out.printf("Saturno: %.1f Kg\n", seuPeso * 1.15);
-        System.out.printf("Urano: %.1f Kg\n", seuPeso * 1.17);
-        System.out.printf("Netuno: %.1f Kg\n", seuPeso * 1.18);
-        System.out.printf("Plutao: %.1f Kg\n", seuPeso * 0.11);
+        System.out.printf("Mercúrio: %.1f Kg\n", peso * 0.37);
+        System.out.printf("Vênus: %.1f Kg\n", peso * 0.88);
+        System.out.printf("Terra: %.1f Kg\n", peso);
+        System.out.printf("Lua: %.1f Kg\n", peso * 0.1);
+        System.out.printf("Marte: %.1f Kg\n", peso * 0.38);
+        System.out.printf("Júpiter: %.1f Kg\n", peso * 2.64);
+        System.out.printf("Saturno: %.1f Kg\n", peso * 1.15);
+        System.out.printf("Urano: %.1f Kg\n", peso * 1.17);
+        System.out.printf("Netuno: %.1f Kg\n", peso * 1.18);
+        System.out.printf("Plutao: %.1f Kg\n", peso * 0.11);
 
         entrada.close ();
     }
