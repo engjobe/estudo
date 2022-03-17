88ac67b1263a8d862c55b4f2d85f33f42ded191b
Joao <engjobe@gmail.com>
2022-03-17 10:25:14 -0300

    Laços de Repetição

---
+++ estudo/LacosRepeticao.java
@@ -0,0 +1,19 @@
+package estudo;
+
+public class LacosRepeticao {
+    public static void main(String[] args) {
+
+		//Criando um laço de repetição em While
+		System.out.println("Criando um laço com While");
+		int contador=0;
+		do {
+			System.out.println(contador);
+			contador++; // Incrementador ou seja é aqui que soma sempre mais 1
+		}while(contador<=5); // Condicional 
+		
+		System.out.println("Criando um laço com For");
+		for(int cont=0; cont<=5; cont++) {
+			System.out.println(cont);
+		}	
+	}    
+}
